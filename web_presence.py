3166e9fa95948ebac55a02816d2ea4a8976788af
Agata Filho <agata78@gmail.com>
2016-07-31 22:17:22 -0400

    Add logging.

--- web_presence.py
+++ web_presence.py
@@ -5,15 +5,23 @@ from user_agents import parse
 from urlparse import urlparse
 from collections import defaultdict
 import uuid
+import logging
 import datetime
 import statsd
 import stat_server
 
 
 app = Flask(__name__)
+logging.basicConfig(filename='web_log', level=logging.INFO)
+
+def send_info(stat, param=None): # stat is a func defined in stat_server is a string
+    stat_func = getattr(stat_server, stat)
+    logging.info('logged: {}, {}'.format(stat, param))
+    stat_func(param)
 
 @app.route('/index')
 def index():
+    # send  logs to a `web_log` file
     # Create response object
     response = app.make_response(render_template('index.html', time_spent=0))
     response.headers['Content-Type'] = 'text/html'
@@ -29,34 +37,35 @@ def index():
     future = now + datetime.timedelta(days=90)
     # cookie will expire in 90 days
     response.set_cookie('user_id', value=UUID, expires=future)
-    stat_server.user_id(UUID)
+    send_info('user_id', param=UUID)
 
     # parse `user_agent` from the responce headers
     ua_string = request.headers['user-agent']
     user_agent = parse(ua_string)
 
     device_type = user_agent.device.family
-    stat_server.ua_device(device_type)
+    send_info('ua_device', param=device_type)
 
     browser = user_agent.browser.family
-    stat_server.ua_browser(browser)
+    send_info('ua_browser', param=browser)
 
     is_mobile = user_agent.is_mobile
     if is_mobile:
-        stat_server.mobile()
+        send_info('mobile')
 
     is_touch_capable = user_agent.is_touch_capable
     if is_touch_capable:
-        stat_server.touch_capable()
+        send_info('touch_capable')
 
     if request.headers.get('referer'):
         referer_parsed =  urlparse(request.headers['referer'])
         referer = referer_parsed.netloc.split(':')[0]
-        stat_server.referer(referer)
+        send_info('referer', param=referer)
 
-    stat_server.status_stat(response.status_code)
+    code = response.status_code
+    send_info('status_stat', param=code)
 
-    stat_server.users_stat()
+    send_info('users_stat')
 
 
     return response
